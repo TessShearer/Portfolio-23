73a21c34e8c6865f88b8d0dcfed0e256fa2be276
Tess Shearer <10734104@uvu.edu>
2023-02-22 13:55:46 -0700

    edit about icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import circle from '../../assets/circle.png';
+import typing from '../../assets/typing.png';
 
 function About() {
 
@@ -6,6 +8,10 @@ function About() {
     <section>
       <div>
         <h1 id="aboutme">Front-end web developer who Designs projects with a goal to make the experience intuitive and simplistic for the user, combining design and functionality of the web page to do so.</h1>
+        <div id="aboutimg">
+          <img id="circle" src={circle} />
+          <img id="typing" src={typing} />
+        </div>
       </div>
     </section>
   );
