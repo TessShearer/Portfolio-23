872373e15de14249dc5a8f8f6aee9bd5041e22df
Tess Shearer <10734104@uvu.edu>
2023-03-28 12:48:44 -0600

    add css and image to about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,8 +5,14 @@ function About() {
   return (
     <section>
       <div>
-        <h1 className="d-block" id="aboutme">Full stack web developer who designs projects with a goal to make the experience intuitive and simplistic for the user.</h1>
-        <img src="../../assets/self-trim.png" alt="Tess Shearer" id="self-img" className="d-block"></img>
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-8">
+            <h1 className="d-block" id="aboutme">Full stack web developer who designs projects with a goal to make the experience intuitive and simplistic for the user.</h1>
+          </div>
+          <div className="col-2"></div>
+        </div>
+        <div id="self"></div>
       </div>
 
       <div className="spacing"></div>
