cd121298dcc69e950a2d1fbdcd11a1b9771fe346
Tess Shearer <10734104@uvu.edu>
2023-03-31 10:00:38 -0600

    Added server stuff, broke everything

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -16,4 +17,35 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'tessshearerportfolio@gmail.com',
+    pass: 'PortPass12'
+  }
+});
+
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create the email
+  const mailOptions = {
+    from: email,
+    to: 'tessshearerportfolio@gmail.com',
+    subject: `New message from ${name}`,
+    text: message
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  });
 });
