4fe8f00a8aa5193c76ee5725eeee76f4e3307a64
Tess Shearer <10734104@uvu.edu>
2023-04-03 09:23:20 -0600

    added new projects to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -35,9 +35,9 @@ function Portfolio() {
         </div>
         <div className="col-5 port-app">
           <h3 className="d-block app-title">Wordle Filter</h3>
-          <a className="d-block app-link" href="https://tessshearer.github.io/5letters/" target="_blank" rel="noreferrer" id="wordle"> </a>
-          <p className="d-block app-des">Personal Development Activity</p>
-          <a className="d-block app-des" href="https://github.com/TessShearer/5letters" target="_blank" rel="noreferrer">Access GitHub Here</a>
+          <a className="d-block app-link" href="https://tims.uvu.edu/tims/prod/myleave.php" target="_blank" rel="noreferrer" id="tims"> </a>
+          <p className="d-block app-des">Updated the UVU time information management system so that employees had a more usuable interface on mobile devices.</p>
+          {/* <a className="d-block app-des" href="https://github.com/TessShearer/5letters" target="_blank" rel="noreferrer">Access GitHub Here</a> */}
         </div>
         <div className="col-1"></div>
       </div>
@@ -45,15 +45,15 @@ function Portfolio() {
       <div className="row text-center">
         <div className="col-1"></div>
         <div className="col-5 port-app">
-          <h3 className="d-block app-title">Movie Search Engine</h3>
-          <a className="d-block app-link" href="https://pacific-anchorage-44756.herokuapp.com/" target="_blank" rel="noreferrer" id="movie"> </a>
-          <p className="d-block app-des">Fill in Later</p>
-          <a className="d-block app-des" href="https://github.com/TessShearer/what-to-watch" target="_blank" rel="noreferrer">Access GitHub Here</a>
+          <h3 className="d-block app-title">UVU Art Museum</h3>
+          <a className="d-block app-link" href="https://ais-apps.uvu.edu/art/lm/signup.php" target="_blank" rel="noreferrer" id="art"> </a>
+          <p className="d-block app-des">Worked as a front end developer to create a sign up form to visit the UVU art musuem, with additional functionality for admins.</p>
+          {/* <a className="d-block app-des" href="https://github.com/TessShearer/what-to-watch" target="_blank" rel="noreferrer">Access GitHub Here</a> */}
         </div>
         <div className="col-5 port-app">
           <h3 className="d-block app-title">Wordle Filter</h3>
           <a className="d-block app-link" href="https://tessshearer.github.io/5letters/" target="_blank" rel="noreferrer" id="wordle"> </a>
-          <p className="d-block app-des">Fill in Later</p>
+          <p className="d-block app-des">Personal development activity that sorts through possible answers for the popular game Wordle.</p>
           <a className="d-block app-des" href="https://github.com/TessShearer/5letters" target="_blank" rel="noreferrer">Access GitHub Here</a>
         </div>
         <div className="col-1"></div>
